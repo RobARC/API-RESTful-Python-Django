e7c5f662480a54ed2e9bfa256ac1caaa3709959d
Roberth <rrondoc@gmail.com>
2021-09-07 14:42:32 -0500

    update add swagger

--- ESW/settings.py
+++ ESW/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ESWAPP',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- ESW/urls.py
+++ ESW/urls.py
@@ -14,10 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include, include
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+"""
+    This loads the content of the paths to the swagger
+"""
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Backend API REST",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="rrondonc@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('', include('ESWAPP.api.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('api/api.json', schema_view.without_ui(cache_timeout=0),
+         name='schema-swagger-ui'),
+
+    path('api/v1/', include('ESWAPP.api.urls')),
 ]
